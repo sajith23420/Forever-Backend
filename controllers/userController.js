fc93c8362564738b1e53c21e78a4672b02b7f6e3
Sajith Bandara <140834328+sajith23420@users.noreply.github.com>
2025-09-03 01:21:42 +0530

    Create the login API

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,8 +13,30 @@ const createToken = (id) => {
 const loginUser = async (req, res) => {
     try{
 
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.json({ success: false, message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            const token = createToken(user._id);
+            res.json({ success: true, token });
+        } else {
+            res.json({ success: false, message: "Invalid credentials" });
+        }
+    
+
+        const token = createToken(user._id);
+        res.json({ success: true, token });
+
     }catch(error){
-        
+
+        console.log(error);
+        res.json({ success: false, message: error.message });
     }
 
 }
